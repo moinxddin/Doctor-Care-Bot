1756701f576da3a025dbc45eefdd1566afd6ec90
mdmoin19 <mohammed.moi@northeastern.edu>
2025-01-29 18:53:00 -0500

    Create ollama_rag.py

---
+++ ollama_rag.py
@@ -0,0 +1,81 @@
+import streamlit as st
+
+import ollama
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OllamaEmbeddings
+#from langchain.document_loaders import PyPDFLoader
+
+
+web = WebBaseLoader("http://www.drwaheedskincare.com")
+docs = web.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+
+# Create Ollama embeddings and vector store
+embeddings = OllamaEmbeddings(model="llama3")
+vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+
+# Create the retriever
+retriever = vectorstore.as_retriever()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+## function to get the output in stream that is chunk by chunk
+def stream_parser(stream):
+    for chunk in stream:
+        yield chunk['message']['content']
+
+# Define the Ollama LLM function
+def ollama_llm(question, context):
+    formatted_prompt = f"Question: {question}\n\nContext: {context}"
+    response = ollama.chat(model='llama3', messages=[{'role': 'user', 'content': formatted_prompt}],stream = True)
+    return response
+
+# Define the RAG chain
+def rag_chain(question):
+    retrieved_docs = retriever.invoke(question)
+    formatted_context = format_docs(retrieved_docs)
+    return ollama_llm(question, formatted_context)
+
+
+
+st.title("Doctor Care Bot")
+
+
+
+
+# initialization of the session state
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+if user_prompt := st.chat_input("What would you like to ask?"):
+    # Display user prompt in chat message widget
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
+
+    # adds user's prompt to session state
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+
+    with st.spinner('Generating response...'):
+        # retrieves response from model
+        llm_stream =rag_chain(user_prompt)
+
+        # streams the response back to the screen
+        stream_output = st.write_stream(stream_parser(llm_stream))
+
+        # appends response to the message list
+        st.session_state.messages.append({"role": "assistant", "content": stream_output})
+
+
